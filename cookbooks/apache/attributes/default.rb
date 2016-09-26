d0b168e82a187870468b9af4d218eb8dc56f8719
root <mounishagona@gmail.com>
2016-09-26 04:18:53 +0000

    first commit

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,3 @@
+default ["apache"]["sites"]["pinky2"] = { "side_title" => "Pinky2 website is on the way", "port" => 80, "domain" => "pinky2.mylabserver.com" } 
+default ["apache"]["sites"]["pinky2b"] = { "side_title" => "Pinky2b website is on the way", "port" => 80, "domain" => "pinky2b.mylabserver.com" } 
+
